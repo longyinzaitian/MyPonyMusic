0b8c19538135b0ca15e9be28097b820fc3d900b4
husyin <nyyin@qq.com>
2018-03-26 10:16:18 +0800

    fork ponymusic init commit

---
+++ app/src/main/java/me/wcy/music/activity/MusicActivity.java
@@ -0,0 +1,313 @@
+package me.wcy.music.activity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import me.wcy.music.R;
+import me.wcy.music.adapter.FragmentAdapter;
+import me.wcy.music.constants.Extras;
+import me.wcy.music.constants.Keys;
+import me.wcy.music.executor.ControlPanel;
+import me.wcy.music.executor.NaviMenuExecutor;
+import me.wcy.music.executor.WeatherExecutor;
+import me.wcy.music.fragment.LocalMusicFragment;
+import me.wcy.music.fragment.PlayFragment;
+import me.wcy.music.fragment.SheetListFragment;
+import me.wcy.music.service.AudioPlayer;
+import me.wcy.music.service.QuitTimer;
+import me.wcy.music.storage.preference.Preferences;
+import me.wcy.music.utils.PermissionReq;
+import me.wcy.music.utils.SystemUtils;
+import me.wcy.music.utils.ToastUtils;
+import me.wcy.music.utils.binding.Bind;
+
+public class MusicActivity extends BaseActivity implements View.OnClickListener, QuitTimer.OnTimerListener,
+        NavigationView.OnNavigationItemSelectedListener, ViewPager.OnPageChangeListener {
+    @Bind(R.id.drawer_layout)
+    private DrawerLayout drawerLayout;
+    @Bind(R.id.navigation_view)
+    private NavigationView navigationView;
+    @Bind(R.id.iv_menu)
+    private ImageView ivMenu;
+    @Bind(R.id.iv_search)
+    private ImageView ivSearch;
+    @Bind(R.id.tv_local_music)
+    private TextView tvLocalMusic;
+    @Bind(R.id.tv_online_music)
+    private TextView tvOnlineMusic;
+    @Bind(R.id.viewpager)
+    private ViewPager mViewPager;
+    @Bind(R.id.fl_play_bar)
+    private FrameLayout flPlayBar;
+
+    private View vNavigationHeader;
+    private LocalMusicFragment mLocalMusicFragment;
+    private SheetListFragment mSheetListFragment;
+    private PlayFragment mPlayFragment;
+    private ControlPanel controlPanel;
+    private NaviMenuExecutor naviMenuExecutor;
+    private MenuItem timerItem;
+    private boolean isPlayFragmentShow;
+
+    private boolean isShaking = false;
+    private SensorManager mSensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+    }
+
+    @Override
+    protected void onServiceBound() {
+        setupView();
+        updateWeather();
+        controlPanel = new ControlPanel(flPlayBar);
+        naviMenuExecutor = new NaviMenuExecutor(this);
+        AudioPlayer.get().addOnPlayEventListener(controlPanel);
+        QuitTimer.get().setOnTimerListener(this);
+        parseIntent();
+        mSensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager != null) {
+            mSensorManager.registerListener(mSensorEventListener,
+                    mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                    SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    /**  感应器的时间监听器     */
+    private SensorEventListener mSensorEventListener =
+            new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    if (isShaking){
+                        return;
+                    }
+
+                    if (Sensor.TYPE_ACCELEROMETER == event.sensor.getType()) {
+                        float[] values = event.values;
+                        int yuZhi = 8;
+                        //监听三个方向上的变化，数据变化剧烈，next()方法播放下一首歌曲*/
+                        if (Math.abs(values[0]) > yuZhi && Math.abs(values[1]) > yuZhi
+                                && Math.abs(values[2]) > yuZhi) {
+                            isShaking = true;
+                            if (Preferences.getShakeMusicEnable()) {
+                                AudioPlayer.get().next();
+                            }
+                            // 延迟200毫秒 防止抖动
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    isShaking = false;
+                                }
+                            }, 200);
+                        }
+                    }
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                }
+            };
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        parseIntent();
+    }
+
+    private void setupView() {
+        // add navigation header
+        vNavigationHeader = LayoutInflater.from(this).inflate(R.layout.navigation_header, navigationView, false);
+        navigationView.addHeaderView(vNavigationHeader);
+
+        // setup view pager
+        mLocalMusicFragment = new LocalMusicFragment();
+        mSheetListFragment = new SheetListFragment();
+        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
+        adapter.addFragment(mLocalMusicFragment);
+        adapter.addFragment(mSheetListFragment);
+        mViewPager.setAdapter(adapter);
+        tvLocalMusic.setSelected(true);
+
+        ivMenu.setOnClickListener(this);
+        ivSearch.setOnClickListener(this);
+        tvLocalMusic.setOnClickListener(this);
+        tvOnlineMusic.setOnClickListener(this);
+        flPlayBar.setOnClickListener(this);
+        mViewPager.addOnPageChangeListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void updateWeather() {
+        PermissionReq.with(this)
+                .permissions(Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                .result(new PermissionReq.Result() {
+                    @Override
+                    public void onGranted() {
+                        new WeatherExecutor(MusicActivity.this, vNavigationHeader).execute();
+                    }
+
+                    @Override
+                    public void onDenied() {
+                        ToastUtils.show(R.string.no_permission_location);
+                    }
+                })
+                .request();
+    }
+
+    private void parseIntent() {
+        Intent intent = getIntent();
+        if (intent.hasExtra(Extras.EXTRA_NOTIFICATION)) {
+            showPlayingFragment();
+            setIntent(new Intent());
+        }
+    }
+
+    @Override
+    public void onTimer(long remain) {
+        if (timerItem == null) {
+            timerItem = navigationView.getMenu().findItem(R.id.action_timer);
+        }
+        String title = getString(R.string.menu_timer);
+        timerItem.setTitle(remain == 0 ? title : SystemUtils.formatTime(title + "(mm:ss)", remain));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_menu:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.iv_search:
+                startActivity(new Intent(this, SearchMusicActivity.class));
+                break;
+            case R.id.tv_local_music:
+                mViewPager.setCurrentItem(0);
+                break;
+            case R.id.tv_online_music:
+                mViewPager.setCurrentItem(1);
+                break;
+            case R.id.fl_play_bar:
+                showPlayingFragment();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawers();
+        handler.postDelayed(() -> item.setChecked(false), 500);
+        return naviMenuExecutor.onNavigationItemSelected(item);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if (position == 0) {
+            tvLocalMusic.setSelected(true);
+            tvOnlineMusic.setSelected(false);
+        } else {
+            tvLocalMusic.setSelected(false);
+            tvOnlineMusic.setSelected(true);
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+    }
+
+    private void showPlayingFragment() {
+        if (isPlayFragmentShow) {
+            return;
+        }
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.setCustomAnimations(R.anim.fragment_slide_up, 0);
+        if (mPlayFragment == null) {
+            mPlayFragment = new PlayFragment();
+            ft.replace(android.R.id.content, mPlayFragment);
+        } else {
+            ft.show(mPlayFragment);
+        }
+        ft.commitAllowingStateLoss();
+        isPlayFragmentShow = true;
+    }
+
+    private void hidePlayingFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.setCustomAnimations(0, R.anim.fragment_slide_down);
+        ft.hide(mPlayFragment);
+        ft.commitAllowingStateLoss();
+        isPlayFragmentShow = false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPlayFragment != null && isPlayFragmentShow) {
+            hidePlayingFragment();
+            return;
+        }
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers();
+            return;
+        }
+
+        super.onBackPressed();
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(Keys.VIEW_PAGER_INDEX, mViewPager.getCurrentItem());
+        mLocalMusicFragment.onSaveInstanceState(outState);
+        mSheetListFragment.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(final Bundle savedInstanceState) {
+        mViewPager.post(() -> {
+            mViewPager.setCurrentItem(savedInstanceState.getInt(Keys.VIEW_PAGER_INDEX), false);
+            mLocalMusicFragment.onRestoreInstanceState(savedInstanceState);
+            mSheetListFragment.onRestoreInstanceState(savedInstanceState);
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        AudioPlayer.get().removeOnPlayEventListener(controlPanel);
+        QuitTimer.get().setOnTimerListener(null);
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(mSensorEventListener);
+        }
+        super.onDestroy();
+    }
+}
